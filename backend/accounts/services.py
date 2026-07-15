91b773e894bb522e3e4ca6552c6a50bbc499ad19
programaan <programaan.exe@gmail.com>
2026-07-15 17:23:33 +0530

    temporary change services

--- backend/accounts/services.py
+++ backend/accounts/services.py
@@ -1,70 +1,11 @@
+from django.core.mail import send_mail
 from django.conf import settings
-from django.template.loader import render_to_string
-from django.core.mail import EmailMultiAlternatives
-import logging
-
-logger = logging.getLogger(__name__)
-
-
-def send_reset_email(email, reset_link):
-    subject = "Reset your LUMEN password"
-
-    html = render_to_string(
-        "emails/reset_password.html",
-        {
-            "reset_link": reset_link,
-        },
-    )
-
-    message = EmailMultiAlternatives(
-        subject=subject,
-        body="",
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        to=[email],
-    )
-
-    message.attach_alternative(
-        html,
-        "text/html",
-    )
-
-    try:
-        message.send(fail_silently=False)
-
-    except Exception:
-        logger.exception(
-            "Failed to send reset password email."
-        )
-        raise
-
 
 def send_verification_email(email, verify_link):
-    subject = "Verify your LUMEN account"
-
-    html = render_to_string(
-        "emails/verify_email.html",
-        {
-            "verify_link": verify_link,
-        },
-    )
-
-    message = EmailMultiAlternatives(
-        subject=subject,
-        body="",
+    send_mail(
+        subject="Verify your LUMEN account",
+        message=f"Click here to verify:\n\n{verify_link}",
         from_email=settings.DEFAULT_FROM_EMAIL,
-        to=[email],
+        recipient_list=[email],
+        fail_silently=False,
     )
-
-    message.attach_alternative(
-        html,
-        "text/html",
-    )
-
-    try:
-        message.send(fail_silently=False)
-
-    except Exception:
-        logger.exception(
-            "Failed to send verification email."
-        )
-        raise
