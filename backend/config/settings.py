6b4868c5d8b5f5002d4c536a8269cca98ce641dd
programaan <programaan.exe@gmail.com>
2026-07-16 02:53:15 +0530

    Switch email to Brevo SMTP

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -174,14 +174,11 @@ IMAGEKIT_URL_ENDPOINT = os.getenv("IMAGEKIT_URL_ENDPOINT")
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-
-EMAIL_PORT = 587
-
-EMAIL_USE_TLS = True
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
